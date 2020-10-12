72dcbb3803d4ab6df0da3c5be43ad25e63bb6414
mboudet <mateo.boudet@gmail.com>
2020-10-12 16:10:12 +0000

    Testing for pathway/reactions

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 import json
 import time
+from flask import request
 from functools import wraps
 from SPARQLWrapper import SPARQLWrapper, JSON
 
@@ -11,24 +12,17 @@ app = Blueprint('app', __name__, url_prefix='/')
 @app.route('/')
 @app.route('/index')
 def index():
-    sparql = SPARQLWrapper("http://0.0.0.0:80/virtuoso/sparql")
-    sparql.setQuery("""
-        PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-        SELECT DISTINCT ?label WHERE {
-            ?gene1_uri rdf:type <http://askomics.org/data/gene> . ?gene1_uri rdfs:label ?label .
-        }
-        LIMIT 10
-    """)
-    sparql.setReturnFormat(JSON)
-    results = sparql.query().convert()
 
+    results = get_genes(count=request.args.get('count'))
     return render_template('home.html', title='Home', data=results["results"]["bindings"])
 
 
-@app.route('/groups/view/<id>', methods=['GET', 'POST'])
-def view_group(id):
-    pass
+@app.route('/genes/<id>', methods=['GET'])
+def get_gene_pathways(id):
+    data = get_gene_data(id)
+    return render_template('gene.html', title='Gene', data=data)
+
+
 
 
 @app.route('/groups/create', methods=['GET', 'POST'])
@@ -38,3 +32,60 @@ def create_group():
 @app.route('/groups/<group_id>/add_orga_access/<orga_id>', methods=['GET', 'POST'])
 def add_orga_group(group_id, orga_id):
     pass
+
+
+
+def get_genes(count=0, start=""):
+    sparql = SPARQLWrapper("http://0.0.0.0:80/virtuoso/sparql")
+
+    start_query = ""
+    if start:
+        start_query = "FILTER(strstarts(str(?label), '{}'))".format(start)
+    count_query = ""
+    if count:
+        count_query = "OFFSET {}".format(count)
+
+    query = """
+        PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+        SELECT DISTINCT ?label WHERE {
+            ?gene1_uri rdf:type <http://askomics.org/data/gene> . ?gene1_uri rdfs:label ?label .
+            %s
+        }
+        LIMIT 100
+        %s
+    """ % (start_query, count_query)
+
+    sparql.setQuery(query)
+    sparql.setReturnFormat(JSON)
+    results = sparql.query().convert()
+
+    return results
+
+
+# Get all results? Might take some time? -> Paginate?
+def get_gene_data(gene_id):
+    sparql = SPARQLWrapper("http://0.0.0.0:80/virtuoso/sparql")
+    sparql.setQuery("""
+        PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+        SELECT DISTINCT ?gene1_Label ?reaction1_Label ?pathway1_Label ?pathway1_COMMON_NAME WHERE {
+            ?rxn_gene_recData16_uri <http://askomics.org/data/is_linked_to> ?gene1_uri .
+            ?rxn_gene_recData16_uri <http://askomics.org/data/concerns> ?reaction25_uri .
+            ?rxn_pwy61_uri <http://askomics.org/data/concerns> ?reaction25_uri .
+            ?rxn_pwy61_uri <http://askomics.org/data/is_included_in> ?pathway80_uri .
+            ?gene1_uri rdf:type <http://askomics.org/data/gene> .
+            ?gene1_uri rdfs:label ?gene1_Label .
+            ?rxn_gene_recData16_uri rdf:type <http://askomics.org/data/rxn_gene_recData> .
+            ?reaction25_uri rdf:type <http://askomics.org/data/reaction> .
+            ?reaction25_uri rdfs:label ?reaction1_Label .
+            ?rxn_pwy61_uri rdf:type <http://askomics.org/data/rxn_pwy> .
+            ?pathway80_uri rdf:type <http://askomics.org/data/pathway> .
+            ?pathway80_uri rdfs:label ?pathway1_Label .
+            ?pathway80_uri <http://askomics.org/data/COMMON-NAME> ?pathway1_COMMON_NAME .
+            VALUES ?gene1_Label { '%s' } .
+        } """ % (gene_id))
+
+    sparql.setReturnFormat(JSON)
+    results = sparql.query().convert()
+    return results["results"]["bindings"]
